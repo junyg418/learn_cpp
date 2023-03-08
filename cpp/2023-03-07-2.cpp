fffca75254184f0cc8f23d025e5607b097e987e8
KIMYO9 <fgh33678@gmail.com>
2023-03-09 00:51:03 +0900

    코드 깔끔하게 변경 후 교수님 피드백으로 코드 반영

--- cpp/2023-03-07-2.cpp
+++ cpp/2023-03-07-2.cpp
@@ -1,27 +1,28 @@
-// 코드가 좀 이상함
+// 코드가 좀 이상함 해결해씀
 # include <iostream>
 
 int main(int argc, char const *argv[])
 {
     int val1, val2;     
-    std::cout << "시작할 숫자와 끝 숫자를 숫자를 입력하세요!\n";
+    std::cout << "first and end input data!\n";
     std::cin >> val1 >> val2;
+
+    if(val1 > val2){
+        int a = val2;
+        val2 = val1;
+        val1 = a;
+    }
+    else if(val1 == val2){
+        std::cout << "no data";
+        return 0;
+    }
     int result;
     if(val1<val2){
-        for (int z = val1+1; z > val2; z++)
+        for (int i = val1 +1; i < val2; i++)
         {
             /* code */
-            result +=z;
+            result +=i;
         }
-    }
-    else if (val2>val1)
-    {
-        for(int i = val2+1; i>val1; i++){
-            result+= i;
-        }
-    }
-    else{
-        std::cout << "값이 없습니다!"<<std::endl;
     }
     std::cout << result;
     
